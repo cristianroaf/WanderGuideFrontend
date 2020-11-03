2e9d7e20a820948f91c9e45c4fcc1d0d3bf59114
Cristian Roa Fortes <roa98@hotmail.com>
2020-11-03 20:56:13 +0100

    Agregar archivos de proyecto.

---
+++ WanderGuideFrontend/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using WanderGuideFrontend.ViewModels;
+
+namespace WanderGuideFrontend.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
