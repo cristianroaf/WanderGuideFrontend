f1e71165bf0a710d27705efe1ed2a787ff52fe31
Cristian Roa Fortes <roa98@hotmail.com>
2020-11-22 21:07:03 +0100

    Added guide and interest point models

---
+++ WanderGuideFrontend/Models/Guide.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WanderGuideFrontend.Models
+{
+    public class Guide
+    {
+        public string _id { get; set; }
+        public bool published { get; set; }
+        public double rating { get; set; }
+        public int total_votes { get; set; }
+        public string user_id { get; set; }
+        public string creator_username { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string title { get; set; }
+        public int __v { get; set; }
+
+    }
+}
---
+++ WanderGuideFrontend/Models/Stop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WanderGuideFrontend.Models
+{
+    public class Stop
+    {
+        public string _id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string guide_id { get; set; }
+        public int __v { get; set; }
+
+        public override string ToString()
+        {
+            return title;
+        }
+    }
+
+}
